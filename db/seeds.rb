084fef088b258cb28103f6f76154f165bb00ec54
Robson Lourenco <robson.lourenco@gmail.com>
2016-07-28 23:46:04 +0000

    Heroku user seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,8 @@
 user = CreateAdminService.new.call
 puts 'CREATED ADMIN USER: ' << user.email
 
+User.create! name: "Robson", email: "robson.lourenco@gmail.com", admin: true, password: "123456"
+
 Portion.delete_all
 Portion.create! description: "Unit"
 Portion.create! description: "Dozen"
@@ -21,17 +23,17 @@ OrderStatus.create! description: "Placed"
 OrderStatus.create! description: "Shipped"
 OrderStatus.create! description: "Cancelled"
 
-Product.delete_all
-Product.create! title: "Brigadeiro", unit_price: 50.49, active: true, portion_id: 4
-Product.create! title: "Cajuzinho", unit_price: 45.29, active: true, portion_id: 4
-Product.create! title: "Bolo de Chocolate", unit_price: 55.99, active: true, portion_id: 1
-Product.create! title: "Bolo de Coco", unit_price: 55.99, active: true, portion_id: 1
-Product.create! title: "Bolo de Nutela", unit_price: 80.99, active: true, portion_id: 1
-Product.create! title: "Bolo de Banana", unit_price: 55.99, active: true, portion_id: 1
-Product.create! title: "Bolo de Abacaxi", unit_price: 55.99, active: true, portion_id: 1
-Product.create! title: "Torta Prestigio", unit_price: 55.99, active: true, portion_id: 1
-Product.create! title: "Torta Crocante", unit_price: 55.99, active: true, portion_id: 1
-Product.create! title: "Torta de Morango com Nutela", unit_price: 55.99, active: true, portion_id: 1
-Product.create! title: "Torta de Coco", unit_price: 55.99, active: true, portion_id: 1
-Product.create! title: "Torta de Nozes", unit_price: 55.99, active: true, portion_id: 1
-Product.create! title: "Torta de Chocolate", unit_price: 55.99, active: true, portion_id: 1
+# Product.delete_all
+# Product.create! title: "Brigadeiro", unit_price: 50.49, active: true, portion_id: 4
+# Product.create! title: "Cajuzinho", unit_price: 45.29, active: true, portion_id: 4
+# Product.create! title: "Bolo de Chocolate", unit_price: 55.99, active: true, portion_id: 1
+# Product.create! title: "Bolo de Coco", unit_price: 55.99, active: true, portion_id: 1
+# Product.create! title: "Bolo de Nutela", unit_price: 80.99, active: true, portion_id: 1
+# Product.create! title: "Bolo de Banana", unit_price: 55.99, active: true, portion_id: 1
+# Product.create! title: "Bolo de Abacaxi", unit_price: 55.99, active: true, portion_id: 1
+# Product.create! title: "Torta Prestigio", unit_price: 55.99, active: true, portion_id: 1
+# Product.create! title: "Torta Crocante", unit_price: 55.99, active: true, portion_id: 1
+# Product.create! title: "Torta de Morango com Nutela", unit_price: 55.99, active: true, portion_id: 1
+# Product.create! title: "Torta de Coco", unit_price: 55.99, active: true, portion_id: 1
+# Product.create! title: "Torta de Nozes", unit_price: 55.99, active: true, portion_id: 1
+# Product.create! title: "Torta de Chocolate", unit_price: 55.99, active: true, portion_id: 1
